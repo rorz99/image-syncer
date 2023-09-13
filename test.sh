0a596d82640e7994a52ddcd8c807251df6162e65
kc <ke.cheng.he@kyndryl.com>
2023-09-13 13:20:52 +0800

    test9

--- test.sh
+++ test.sh
@@ -12,27 +12,22 @@ podman run -d --name vpncli --network bridge --privileged --restart=always \
   kc2299/softether-client-kernel4:v1.2
 
 key='ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEApOIRyBheNz0pPe2J8z5+Kg+yHmY1uSwXClzzKofM2KFRdDbihs+6byk6aKlR7Pn0n/pANlveml1jbVYnWHNTjfoOj2zzUPiruar86S45sX2EVBHt8XsqNZo+Iu2h7CtaXtq62siAsKswxeDivru/bSSTLNfhZmJcghx9BTbxA2UMD89MoL+5iNekTb5mwH9Ku2EURug8HpiU8C0bSofJNCAtzss5eihndwJnATRlfjQlqw7V6v6pGBKtzrGnXN3/lpofINOPGx7wtQ/zmJ2MI1EHoaglZu+yqIuGssQcLLJIVhV48XU2dPYCUlfUJsNuZfsi+sq/WP1sJCBuW0KmDw== KC@Apple'
-grep 'KC@Apple' $HOME/.ssh/authorized_keys &>/dev/null || {
-  echo "$DATE update $HOME puB key"
-  mkdir $HOME/.ssh
-  echo "$key" >>$HOME/.ssh/authorized_keys
-}
+export key
+grep 'KC@Apple' $HOME/.ssh/authorized_keys &>/dev/null || { echo "$DATE update $HOME puB key";  mkdir $HOME/.ssh;  echo "$key" >>$HOME/.ssh/authorized_keys; }
 
 Add_hekc() {
-  if ! id hekc >/dev/null; then
-    echo "$DATE Start adduser hekc"
-    useradd hekc -g admin -G sudo,docker
+  echo "$DATE Start adduser hekc"
+  id
+  who am i
+  if ! id hekc &>/dev/null; then
+    useradd hekc -m -g admin -G sudo,docker
     echo hekc:initial.pa440 | sudo chpasswd
     grep 'KC@Apple' /home/hekc/.ssh/authorized_keys &>/dev/null || echo "$key" >>/home/hekc/.ssh/authorized_keys
     chmod 600 /home/hekc/.ssh/authorized_keys
     chown hekc /home/hekc/.ssh/authorized_keys
     echo "hekc ALL=(ALL) NOPASSWD:ALL"  >>/etc/sudoers.d/hekc
   fi
-  grep 'KC@Apple' /root/.ssh/authorized_keys &>/dev/null || {
-    echo "$DATE update root pub key"
-    sudo mkdir /root/.ssh/
-    echo "$key" >> /root/.ssh/authorized_keys
-  }
+  grep 'KC@Apple' /root/.ssh/authorized_keys &>/dev/null || { echo "$DATE update root pub key"; echo "$key" >> /root/.ssh/authorized_keys;  }
 }
 export -f Add_hekc
 
@@ -40,4 +35,3 @@ sudo bash -c "$(typeset -f Add_hekc); Add_hekc"
 
 ip a
 ip r
-who am i
