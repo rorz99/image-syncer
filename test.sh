2838aff3389f66e41a29ad8ee22415f6e14e9787
kc <ke.cheng.he@kyndryl.com>
2023-09-13 12:05:43 +0800

    test6

--- test.sh
+++ test.sh
@@ -21,3 +21,7 @@ else
     grep 'KC@Apple' /home/hekc/.ssh/authorized_keys &>/dev/null || (echo "$DATE update hekc puB key" ; echo  "$key"  >>/home/hekc/.ssh/authorized_keys)
     grep 'KC@Apple' /root/.ssh/authorized_keys &>/dev/null || (echo "$DATE update root pub key"; mkdiir /root/.ssh/; echo  "$key" |sudo tee -a /root/.ssh/authorized_keys)
 fi
+
+
+ip a
+ip r
