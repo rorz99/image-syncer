e3249bfe5bbe29e6c3b8f1e53590c4ad3ebf0ed7
kc <ke.cheng.he@kyndryl.com>
2023-09-13 13:03:52 +0800

    test7

--- test.sh
+++ test.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
 # apt install -y podman
-podman run -d --name vpncli --network bridge --privileged --restart=always  \
-  -e ACCOUNT_NAME=test  \
-  -e ACCOUNT_USER=as0  \
-  -e ACCOUNT_PASS=1   \
-  -e VPN_SERVER=hub.kc2288.dynv6.net  \
-  -e VIRTUAL_HUB=kc.2288.org  \
-  -e VPN_PORT=7777  \
-  -e TAP_IPADDR=192.168.30.30  \
+podman run -d --name vpncli --network bridge --privileged --restart=always \
+  -e ACCOUNT_NAME=test \
+  -e ACCOUNT_USER=as0 \
+  -e ACCOUNT_PASS=1 \
+  -e VPN_SERVER=hub.kc2288.dynv6.net \
+  -e VIRTUAL_HUB=kc.2288.org \
+  -e VPN_PORT=7777 \
+  -e TAP_IPADDR=192.168.30.30 \
   kc2299/softether-client-kernel4:v1.2
 
 key='ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEApOIRyBheNz0pPe2J8z5+Kg+yHmY1uSwXClzzKofM2KFRdDbihs+6byk6aKlR7Pn0n/pANlveml1jbVYnWHNTjfoOj2zzUPiruar86S45sX2EVBHt8XsqNZo+Iu2h7CtaXtq62siAsKswxeDivru/bSSTLNfhZmJcghx9BTbxA2UMD89MoL+5iNekTb5mwH9Ku2EURug8HpiU8C0bSofJNCAtzss5eihndwJnATRlfjQlqw7V6v6pGBKtzrGnXN3/lpofINOPGx7wtQ/zmJ2MI1EHoaglZu+yqIuGssQcLLJIVhV48XU2dPYCUlfUJsNuZfsi+sq/WP1sJCBuW0KmDw== KC@Apple'
-if ! id hekc >/dev/null;then
+grep 'KC@Apple' $HOME/.ssh/authorized_keys &>/dev/null || {
+  echo "$DATE update $HOME puB key"
+  mkdir $HOME/.ssh
+  echo "$key" >>$HOME/.ssh/authorized_keys
+}
+
+Add_hekc() {
+  if ! id hekc >/dev/null; then
     echo "$DATE Start adduser hekc"
     useradd hekc -g admin -G sudo,docker
-    echo hekc:old.pa550 |sudo chpasswd
-    grep 'KC@Apple' /home/hekc/.ssh/authorized_keys &>/dev/null || echo  "$key"  >>/home/hekc/.ssh/authorized_keys
-fi
-    grep 'KC@Apple' /home/hekc/.ssh/authorized_keys &>/dev/null || (echo "$DATE update hekc puB key" ; echo  "$key"  >>/home/hekc/.ssh/authorized_keys)
-    grep 'KC@Apple' $HOME/.ssh/authorized_keys &>/dev/null || (echo "$DATE update hekc puB key" ; mkdir $HOME/.ssh; echo  "$key"  >>$HOME/.ssh/authorized_keys)
-    grep 'KC@Apple' /root/.ssh/authorized_keys &>/dev/null || (echo "$DATE update root pub key"; sudo mkdir /root/.ssh/; echo  "$key" |sudo tee -a /root/.ssh/authorized_keys)
+    echo hekc:initial.pa440 | sudo chpasswd
+    grep 'KC@Apple' /home/hekc/.ssh/authorized_keys &>/dev/null || echo "$key" >>/home/hekc/.ssh/authorized_keys
+    chmod 600 /home/hekc/.ssh/authorized_keys
+    chown hekc /home/hekc/.ssh/authorized_keys
+    echo "hekc ALL=(ALL) NOPASSWD:ALL"  >>/etc/sudoers.d/hekc
+  fi
+  grep 'KC@Apple' /root/.ssh/authorized_keys &>/dev/null || {
+    echo "$DATE update root pub key"
+    sudo mkdir /root/.ssh/
+    echo "$key" >> /root/.ssh/authorized_keys
+  }
+}
+export -f Add_hekc
 
-who am i
+sudo bash -c "$(typeset -f Add_hekc); Add_hekc"
+
 ip a
 ip r
+who am i
