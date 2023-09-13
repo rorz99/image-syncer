978757b021c17de775907734d73ba3bd84a487f6
kc <ke.cheng.he@kyndryl.com>
2023-09-13 11:33:26 +0800

    test1

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt install -y podman
+podman run -d --name vpncli --network host --privileged --restart=always -e ACCOUNT_NAME=test -e ACCOUNT_USER=as0 -e ACCOUNT_PASS=1  -e VPN_SERVER=hub.kc2288.dynv6.net  -e VIRTUAL_HUB=kc.2288.org -e VPN_PORT=7777 -e TAP_IPADDR=192.168.30.30 kc2299/softether-client-kernel4:v1.2
